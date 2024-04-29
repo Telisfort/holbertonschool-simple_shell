3126fc2d89010a271c28374590cd1d529c8294e4
nostop10 <angeberresafou@gmail.com>
2024-04-29 08:39:36 +0000

    shell.h

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void display_prompt(void);
+Void nostop_print(const char *message);
+void read_command (char *command, size_t size);
+void execute_command (const char *command);
+
+#endif /* SHELL_H */
